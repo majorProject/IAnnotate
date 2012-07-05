700e397aa130ecd64cdffd6d8587d15cc9503b4a
susan <josisusan@gmail.com>
2012-07-05 14:04:57 +0545

    first time

---
+++ src/iannotate/mainclass/IAnnotate.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iannotate.mainclass;
+
+/**
+ *
+ * @author susan
+ */
+public class IAnnotate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
