c44fd4c25035e1488422a0babab523bf5648e837
susan <josisusan@gmail.com>
2012-07-05 20:22:29 +0545

    added package utility for formatted date conversion

---
+++ src/iannotate/utility/FormattedDate.java
@@ -0,0 +1,33 @@
+/*
+ * 
+ */
+package iannotate.utility;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+/**
+ *
+ * @author susan
+ */
+public class FormattedDate {
+    /**
+     * When the input is 2011:12:11,
+     * then this function gives output as:
+     *  2011 December 11
+     * @param unformattedDate
+     * @return formatted date 
+     */
+    static public String getDate(String unformattedDate){
+        String[] month = unformattedDate.split(":");
+        String result;
+        int dat = Integer.parseInt(month[1]);
+        System.out.println(dat);
+        Calendar a = Calendar.getInstance();
+        a.set(Calendar.MONTH, dat-1);
+        a.set(Calendar.DAY_OF_MONTH,1);
+        String nameOfMonth = a.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        result = month[0]+" "+nameOfMonth+" "+month[2];
+        return result;
+    }
+}
