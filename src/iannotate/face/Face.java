7d214c665d7fc5a8a36ecba3ecf1df5caddb0d0b
RAJAN <RAJAN@RAJAN-PC>
2012-07-06 10:37:44 +0545

    added MainFrame metadata.getDirectory(ExifSubIDDirectory.class was casted to ExifSubIFDDirectory in EXIFMetadataExtractor() in and readImageFile() MetadataException thrown in other functions Constructor EXIFMetadataExtractor was made public contructor Face() and detectFace function with return type Region[] added

--- src/iannotate/face/Face.java
+++ src/iannotate/face/Face.java
@@ -4,10 +4,32 @@
  */
 package iannotate.face;
 
+import de.offis.faint.controller.MainController;
+import de.offis.faint.model.ImageModel;
+import de.offis.faint.model.Region;
+
 /**
  *
  * @author susan
  */
 public class Face {
+    private static String source;
+    private static String destination;
+    private Region[] regions;
     
+    public Face(String src, String dest) {
+        source = src;
+        destination = dest;
+    }
+    
+    public Region[] detectFace() {
+        MainController controller = MainController.getInstance();
+        if(controller != null) {
+            controller.setScanWindowSize(1);
+            ImageModel imageModel = new ImageModel(source);
+            imageModel.initThumbnail();
+            regions = controller.detectFaces(imageModel, false);                        
+        }
+        return regions;
+    }
 }
