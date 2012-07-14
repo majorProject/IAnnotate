d9b8a6e50540c411cdd31f8c7fedf67a0b7b91d3
RAJAN <RAJAN@RAJAN-PC>
2012-07-14 13:36:13 +0545

    Main class updated

---
+++ src/iannotate/mainclass/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iannotate.mainclass;
+
+/**
+ *
+ * @author RAJAN
+ */
+public class Main {
+    
+}
