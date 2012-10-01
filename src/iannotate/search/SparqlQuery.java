c875b632b157a16396aa6a3b3dc2007ab3d850a7
sameer <sameer@192.168.10.107>
2012-10-01 17:38:01 +0545

    description and date extractor function added

--- src/iannotate/search/SparqlQuery.java
+++ src/iannotate/search/SparqlQuery.java
@@ -12,6 +12,9 @@ import iannotate.database.PersonWithRelation;
 import iannotate.database.SearchPersonClass;
 import iannotate.utility.FileExtensionFilter;
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,7 +26,7 @@ public class SparqlQuery {
 
     public static void searchPerson(String path, String name) throws IOException {
         //open the rdf
-        // FilePermission prem = new java.io.FilePermission(".\\rdfs\\", "read");
+       
         File rdf = new File(path);
         Model model;
         InputStream in = new FileInputStream(rdf);
@@ -223,6 +226,10 @@ public class SparqlQuery {
 
     public static String findRelation(String rdf, String person, String user) throws FileNotFoundException, IOException {
 
+        if(person.toLowerCase().equals(user.toLowerCase())) {
+            return "me";
+        }
+        
         // Open the bloggers RDF graph from the filesystem
         InputStream in = new FileInputStream(new File(rdf));
         // Create an empty in-memory model and populate it from the graph
@@ -265,7 +272,16 @@ public class SparqlQuery {
 
         return relation;
     }
-    
+    /**
+     * 
+     * @param rdfPath
+     * @param user
+     * @param relation
+     * @return LinkedList
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+
     public static LinkedList relateToPerson(String rdfPath,String user,String relation) throws FileNotFoundException, IOException {
 
         LinkedList<SearchPersonClass> list = new LinkedList<>();
@@ -316,6 +332,82 @@ public class SparqlQuery {
 
     }
 
+    public static String DateExtractor( String path) throws FileNotFoundException{
+        
+        InputStream in = new FileInputStream( new File( path ));
+        
+        Model model = ModelFactory.createMemModelMaker().createModel(null);
+        
+        model.read( in, null );
+        
+        String queryString = 
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
+                + "PREFIX EXIF: <http://www.w3.org/2003/12/exif/ns#> "
+                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                + "PREFIX relation: <http://purl.org/vocab/relationship/> "
+                + "SELECT ?date "
+                + "WHERE { "
+                + "?node EXIF:date ?date . }";
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qe = QueryExecutionFactory.create(query, model);
+        ResultSet result = qe.execSelect();
+       
+        
+        String date = null;
+        
+        while(result.hasNext()){
+            QuerySolution qs = result.nextSolution();
+            date = qs.get("date").toString();
+           }
+        
+        if (date == null)//if no date define in metadata
+            {
+                DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                //get current date time with Date()
+                Date nDate = new Date();
+                date = dateFormat.format(nDate);
+                System.out.println("there is no date data, so here is the system date:");
+
+                //get current date time with Calendar()
+                //Calendar cal = Calendar.getInstance();
+                //System.out.println(dateFormat.format(cal.getTime()));
+            }
+        qe.close();
+        return date;
+    }
+    
+    public static String DescriptionExtractor( String path) throws FileNotFoundException{
+         //open the rdf
+       
+        File rdf = new File(path);
+        Model model;
+        InputStream in = new FileInputStream(rdf);
+        model = ModelFactory.createMemModelMaker().createModel(null);
+        model.read(in, null);
+        
+        String queryString =
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
+                + "PREFIX EXIF: <http://www.w3.org/2003/12/exif/ns#> "
+                + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+                + "PREFIX relation: <http://purl.org/vocab/relationship/> "
+                + "SELECT ?desc "
+                + "WHERE { "
+                + "?node EXIF:imageDescription ?desc . }";
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qe = QueryExecutionFactory.create(query, model);
+        ResultSet result = qe.execSelect();
+        
+        String description = null;
+        while(result.hasNext()){
+            QuerySolution qs = result.nextSolution();
+            description = qs.get("desc").toString();
+        }
+        
+        qe.close();
+        return description;
+    
+    }
+    
     private static String changeFileExt(String str) {
         String string[];
         string = str.split(".rdf");
